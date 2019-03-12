868667d3290075be0dfd560f6ae22b8d212dca55
evertondewes <everton.dewes@gmail.com>
2019-03-12 20:42:28 -0300

    entrada de dados filtrada

--- formulario1.php
+++ formulario1.php
@@ -1,43 +1,42 @@
 <html>
-<form method="post" >
- Valor 1:<input type="number"  name="valor1" /><br>
- Valor 2:<input type="number"  name="valor2" /><br>
- +<input type="radio" name="operacao" value="+"/>
- -<input type="radio" name="operacao" value="-"/>
- *<input type="radio" name="operacao" value="*"/>
- /<input type="radio" name="operacao" value="/"/><br>
- <input type="submit" name="action" value="Enviar"/><br>
+<form method="post">
+    Valor 1:<input type="number" name="valor1"/><br>
+    Valor 2:<input type="number" name="valor2"/><br>
+    +<input type="radio" name="operacao" value="+"/>
+    -<input type="radio" name="operacao" value="-"/>
+    *<input type="radio" name="operacao" value="*"/>
+    /<input type="radio" name="operacao" value="/"/><br>
+    <input type="submit" name="action" value="Enviar"/><br>
 </form>
 </body>
 </html>
 <?php
 
-if(!isset($_POST['valor1']) || trim($_POST['valor1'])==""){
-    echo 'Valor 1 não foi informado<br>';
+$valor1 = filter_input(INPUT_POST, 'valor1', FILTER_VALIDATE_INT);
+$valor2 = filter_input(INPUT_POST, 'valor2', FILTER_VALIDATE_INT);
+
+if (!isset($_POST['operacao'])) {
+    echo 'Operação não escolhida';
 } else {
-    if(!isset($_POST['valor2']) || trim($_POST['valor2'])=="") {
-        echo 'Valor 2 não foi informado<br>';
-    } else {
-        if(!isset($_POST['operacao'])) {
-            echo 'Operação não escolhida';
-        } else {
-            switch ($_POST['operacao']) {
-                case '+': echo $_POST['valor1'] + $_POST['valor2'];
-                    break;
-                case '-': echo $_POST['valor1'] - $_POST['valor2'];
-                    break;
-                case '*': echo $_POST['valor1'] * $_POST['valor2'];
-                    break;
-                case '/':
-                        if($_POST['valor2'] == 0) {
-                            echo 'Proibida a divisão por zero';
-                        } else {
-                            echo $_POST['valor1'] / $_POST['valor2'];
-                        }
-                    break;
+    switch ($_POST['operacao']) {
+        case '+':
+            echo $valor1 + $valor2;
+            break;
+        case '-':
+            echo $valor1 - $valor2;
+            break;
+        case '*':
+            echo $valor1 * $valor2;
+            break;
+        case '/':
+            if ($valor2 == 0) {
+                echo 'Proibida a divisão por zero';
+            } else {
+                echo $valor1 / $valor2;
             }
-        }
+            break;
     }
+
 }
 //echo '<pre>' . print_r($_POST, true). '</pre>';
 
