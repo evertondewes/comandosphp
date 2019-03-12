c0721f84926deb709a31ac1b7d32d56e70a06c03
evertondewes <everton.dewes@gmail.com>
2019-03-11 21:57:37 -0300

    alterado formulario 1 para uma calculadora

--- formulario1.php
+++ formulario1.php
@@ -1,20 +1,44 @@
 <html>
 <form method="post" >
- <input type="text"  name="valor" /><br>
- <input type="radio"  name="sexo" value="masculino"/>Masculino<br>
- <input type="radio" name="sexo" value="feminino" checked="checked"/>Fem<br>
- <input type="password"  name="senha" />Senha<br><br>
- <input type="radio" name="estrangeiro" value="sim"/>Estrangeiro<br>
- <input type="radio" name="estrangeiro" value="nao" checked="checked" />Br<br>
+ Valor 1:<input type="number"  name="valor1" /><br>
+ Valor 2:<input type="number"  name="valor2" /><br>
+ +<input type="radio" name="operacao" value="+"/>
+ -<input type="radio" name="operacao" value="-"/>
+ *<input type="radio" name="operacao" value="*"/>
+ /<input type="radio" name="operacao" value="/"/><br>
  <input type="submit" name="action" value="Enviar"/><br>
 </form>
 </body>
 </html>
 <?php
 
-if(isset($_POST['sexo'])){
-    echo 'Variavel existe<br>';
+if(!isset($_POST['valor1']) || trim($_POST['valor1'])==""){
+    echo 'Valor 1 não foi informado<br>';
+} else {
+    if(!isset($_POST['valor2']) || trim($_POST['valor2'])=="") {
+        echo 'Valor 2 não foi informado<br>';
+    } else {
+        if(!isset($_POST['operacao'])) {
+            echo 'Operação não escolhida';
+        } else {
+            switch ($_POST['operacao']) {
+                case '+': echo $_POST['valor1'] + $_POST['valor2'];
+                    break;
+                case '-': echo $_POST['valor1'] - $_POST['valor2'];
+                    break;
+                case '*': echo $_POST['valor1'] * $_POST['valor2'];
+                    break;
+                case '/':
+                        if($_POST['valor2'] == 0) {
+                            echo 'Proibida a divisão por zero';
+                        } else {
+                            echo $_POST['valor1'] / $_POST['valor2'];
+                        }
+                    break;
+            }
+        }
+    }
 }
-echo '<pre>' . print_r($_POST, true). '</pre>';
+//echo '<pre>' . print_r($_POST, true). '</pre>';
 
 ?>
